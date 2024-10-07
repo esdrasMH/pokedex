ba884e36026c044cd060c65d1b34bea8f7f93bbf
Esdras Malave <esdras.malave.04@gmail.com>
2024-10-06 21:19:42 -0300

    Se añadio componente para motrar mensaje cuando no hay datos disponibles

---
+++ pokemon-list/src/components/NoData.jsx
@@ -0,0 +1,11 @@
+import styles from "./../styles/NoData.module.css";
+
+function NoData() {
+  return (
+    <section className={styles["no-data-container"]}>
+      <p>No hay datos disponibles de Pokémones</p>
+    </section>
+  );
+}
+
+export default NoData;
